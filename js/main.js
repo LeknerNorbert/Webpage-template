738dbd84df8463d789c321f35af79cef6c0d543e
LeknerNorbert <leknern@kkszki.hu>
2022-01-26 00:57:15 +0100

    ready

--- js/main.js
+++ js/main.js
@@ -20,9 +20,25 @@ function nav_change()
 
 const mouse_area = document.getElementById('mouse-area')
 const cube = document.getElementById('cube')
+const mouse = document.getElementById('mouse')
+let sound = new Audio('pig_sound_effect.mp3')
 
 mouse_area.addEventListener('mousemove', (event) => {
     cube.style.transform = 'translate3d(' + ((event.offsetX - mouse_area.clientWidth / 2) / 3) +'px, ' + ((event.offsetY - mouse_area.clientHeight / 2) / 3) + 'px, 0)'
+    mouse.style.transform = 'translate3d(' + (event.offsetX - 64 ) + 'px, ' + (event.offsetY - 64) + 'px, 0)'
 })
+
+mouse_area.addEventListener('mouseleave', (event) => {
+    cube.style.transform = 'translate3d(0, 0, 0)'
+    mouse.style.transform = 'translate3d(' + (mouse_area.clientWidth / 2 - 64) + 'px, ' + (mouse_area.clientHeight / 2 - 64) + 'px, 0)'
+})
+
+mouse_area.addEventListener('mousedown', (event) => {
+    sound.play()
+})
+
+
+
+
 
 
